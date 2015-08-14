a65ba4eaf02d78dfe1ede059120bc3fe4c56cca5
UberKitten <566438+UberKitten@users.noreply.github.com>
2015-08-14 00:38:56 -0500

    Add automatic, barebones file replacement for config files

---
+++ homelab-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# sudo bash <(curl -s https://github.com/T3hUb3rK1tten/homelab/raw/master/homelab-setup.sh)
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+rm -Rf /home/astra/.ssh
+mkdir /home/astra/.ssh
+curl -L -o /home/astra/.ssh/authorized_keys https://github.com/T3hUb3rK1tten/homelab/raw/master/authorized_keys
+chown -R astra:astra /home/astra/.ssh
+chmod -R 700 /home/astra/.ssh
+chmod 600 /home/astra/.ssh/authorized_keys
+
+curl -L -o /etc/sudoers.d/astra-sudo https://github.com/T3hUb3rK1tten/homelab/raw/master/astra-sudo
+chmod 440 /etc/sudoers.d/astra-sudo
+
+curl -L -o /etc/ssh/sshd_config https://github.com/T3hUb3rK1tten/homelab/raw/master/sshd_config
+chmod 644 /etc/ssh/sshd_config
+service sshd reload
