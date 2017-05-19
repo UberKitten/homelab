d4be0b5a384cf97fa8de73a2828bd29673f152a4
UberKitten <566438+UberKitten@users.noreply.github.com>
2017-05-19 01:12:08 -0500

    Add Chocolatey scripts

---
+++ Chocolatey-Packages.ps1
@@ -0,0 +1,32 @@
+choco install notepadplusplus.install -y
+choco install 7zip.install -y
+choco install vlc -y
+choco install sysinternals -y
+choco install dropbox -y
+choco install paint.net -y
+choco install teamviewer -y
+choco install kdiff3 -y
+choco install git.install -y
+choco install gitextensions -y
+choco install steam -y
+choco install spotify -y
+choco install sumatrapdf -y
+choco install android-sdk -y
+choco install sudo -y
+choco install mumble -y
+choco install teracopy -y
+choco install processhacker -y
+choco install wudt -y
+choco install deluge -y
+choco install battle.net -y
+choco install goggalaxy -y
+choco install gpg4win -y
+choco install minecraft -y
+choco install origin -y
+choco install rescuetime -y
+choco install skype -y
+choco install wireshark -y
+choco install virtualclonedrive -y
+
+# Not sure if needed?
+# choco install garmin-express -y
