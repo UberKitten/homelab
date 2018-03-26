7a2e745158866980cd3f29c2b711b2d2a944ad56
UberKitten <566438+UberKitten@users.noreply.github.com>
2018-03-26 18:07:07 -0500

    Remove obsolete packages, add additional

--- Chocolatey-Packages.ps1
+++ Chocolatey-Packages.ps1
@@ -11,24 +11,34 @@ choco install gitextensions -y
 choco install steam -y
 choco install spotify -y
 choco install sumatrapdf -y
-choco install android-sdk -y
 choco install sudo -y
 choco install mumble -y
 choco install teracopy -y
 choco install processhacker -y
-choco install wudt -y
 choco install deluge -y
-choco install battle.net -y
+choco install battle.net -y --allow-empty-checksums
 choco install goggalaxy -y
 choco install gpg4win -y
 choco install minecraft -y
 choco install origin -y
-choco install rescuetime -y
+choco install rescuetime -y --ignore-checksums
 choco install skype -y
 choco install wireshark -y
-choco install virtualclonedrive -y
-choco install chirp -y
+choco install chirp.install -y
 choco install powerpanel-personal -y
+choco install windirstat -y
+choco install glogg -y
+choco install fiddler4 -y
+choco install slack -y
+choco install discord -y
+choco install arduino -y
 
-# Not sure if needed?
+# Rarely needed
 # choco install garmin-express -y
+# choco install virtualclonedrive -y
+
+# Obsolete for Windows 10
+# choco install wudt -y 
+
+# Platform-Tools are available separately
+# choco install android-sdk -y
